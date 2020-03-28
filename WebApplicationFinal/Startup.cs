79b85e87c7a827ac053e150a2793054f124d6698
Maxwell <maxwell201414@gmail.com>
2020-03-28 11:16:05 -0300

    MySQL adaptation and first migration

--- WebApplicationFinal/Startup.cs
+++ WebApplicationFinal/Startup.cs
@@ -37,7 +37,8 @@ namespace WebApplicationFinal
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplicationFinalContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplicationFinalContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebApplicationFinalContext"), builder =>
+                        builder.MigrationsAssembly("WebApplicationFinal")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
