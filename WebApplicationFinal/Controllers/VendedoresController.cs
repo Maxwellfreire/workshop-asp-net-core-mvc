8d430d26f6bbdade34706f7011774da32f39ca8e
Maxwell <maxwell201414@gmail.com>
2020-03-30 17:33:20 -0300

    Validation

--- WebApplicationFinal/Controllers/VendedoresController.cs
+++ WebApplicationFinal/Controllers/VendedoresController.cs
@@ -39,6 +39,13 @@ namespace WebApplicationFinal.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Vendedor vendedor)
         {
+
+            if (!ModelState.IsValid)
+            {
+                var departamentos = _departamentoService.FindAll();
+                var viewModel = new VendedorFormViewModel { Vendedor = vendedor, Departamentos = departamentos };
+                return View(viewModel);
+            }
             _vendedorService.Insert(vendedor);
             return RedirectToAction(nameof(Index));
         }
@@ -106,6 +113,12 @@ namespace WebApplicationFinal.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Vendedor vendedor)
         {
+            if (!ModelState.IsValid)
+            {
+                var departamentos = _departamentoService.FindAll();
+                var viewModel = new VendedorFormViewModel { Vendedor = vendedor, Departamentos = departamentos };
+                return View(viewModel);
+            }
             if (id != vendedor.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id não corresponde" });
