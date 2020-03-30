d84c16abffd22c7b9d1f18f6b97779052a2059fd
Maxwell <maxwell201414@gmail.com>
2020-03-30 16:50:53 -0300

    retorn error custom

--- WebApplicationFinal/Controllers/VendedoresController.cs
+++ WebApplicationFinal/Controllers/VendedoresController.cs
@@ -7,6 +7,7 @@ using WebApplicationFinal.Services;
 using WebApplicationFinal.Models;
 using WebApplicationFinal.Models.ViewModels;
 using WebApplicationFinal.Services.Exceptions;
+using System.Diagnostics;
 
 namespace WebApplicationFinal.Controllers
 {
@@ -46,12 +47,12 @@ namespace WebApplicationFinal.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _vendedorService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -69,12 +70,12 @@ namespace WebApplicationFinal.Controllers
 
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _vendedorService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -86,12 +87,12 @@ namespace WebApplicationFinal.Controllers
 
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _vendedorService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Departamento> departamentos = _departamentoService.FindAll();
@@ -107,7 +108,7 @@ namespace WebApplicationFinal.Controllers
         {
             if (id != vendedor.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id não corresponde" });
             }
             
 
@@ -116,19 +117,24 @@ namespace WebApplicationFinal.Controllers
                 _vendedorService.Update(vendedor);
                 return RedirectToAction(nameof(Index));
 
-            } catch (NotFoundException)
+            } catch (ApplicationException e)
             {
 
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
 
             }            
-            catch (DbConcurrencyException)
+            
+
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-
-                return BadRequest();
-
-            }
-
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
 
     }
